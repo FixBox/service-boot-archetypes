332d34338758c226336f819f74d0c4e56b1d0421
FixBox <fixbox@hotmail.it>
2023-12-07 14:33:39 +0100

    feat: added flyway plugin and example to connect db in post

---
+++ src/main/resources/archetype-resources/src/main/java/dto/TestTableDto.java
@@ -0,0 +1,9 @@
+package ${package}.dto;
+
+import lombok.Data;
+
+@Data
+public class TestTableDto {
+    private String id;
+    private String description;
+}
--- src/main/resources/archetype-resources/src/main/java/services/interfaces/ServiceAppl.java
+++ src/main/resources/archetype-resources/src/main/java/services/interfaces/ServiceAppl.java
@@ -1,4 +1,9 @@
 package ${package}.services.interfaces;
+
+import ${package}.dto.TestTableDto;
+import ${package}.jpa.model.TestTableEntity;
 public interface ServiceAppl {
     String getHello();
+
+    TestTableEntity addTestTableDto(TestTableDto testTableDto);
 }
